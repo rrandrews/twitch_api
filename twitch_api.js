67afb78f512e6d4046a387cf0e53c06276145fca
Ron Andrews <ron.andrews@gmail.com>
2016-08-21 07:14:28 -0400

    displays list of streams online

--- twitch_api.js
+++ twitch_api.js
@@ -1,22 +1,53 @@
 streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck",
-            "habathcx", "RobotCaleb", "noobs2ninjas", "LotharHS"];
+            "habathcx", "RobotCaleb", "noobs2ninjas", "LotharHS", "brunofin"];
 
 $(document).ready(function() {
-  queryTwitch(streamers);
+
+  $.when(queryTwitch(streamers)).then(
+    function(data) { updateTable(data); },
+    function(data) { alert(data.status); }
+  );
+
+  //
+  // $.when(queryChannel('brunofin')).then(
+  //   function(data) {
+  //     alert("success");
+  //   }, function(data) {
+  //     alert(data.status);
+  //   }
+  // );
+  //
 });
 
 function queryTwitch(channels) {
   query = "https://api.twitch.tv/kraken/streams?channel=" + channels.join();
   console.log(query);
-  $.getJSON(query, function(data) {
-    $.each(data.streams, function(index, value) {
+  return $.ajax(query);
+}
 
-    })
-  });
+
+function queryChannel(channel) {
+  query = "https://api.twitch.tv/kraken/streams/" + channel;
+  return $.ajax(query);
 }
 
 function updateTable(data) {
-  $.each(streamers, function(index, value) {
-
+  var offlineStreamers = streamers;
+  html = '';
+  $.each(data.streams, function(index, value) {
+    offlineStreamers.splice(offlineStreamers.indexOf(this.channel.display_name), 1);
+    html += '<tr><td><img class="logo" src="' + this.channel.logo + '"></td>' +
+            '<td><a href="' + this.channel.url + '">' +
+            this.channel.display_name + '</a></td>' +
+            '<td>' + this.channel.status + '</td></tr>';
   })
+  $("#streams-tbody").html(html);
+
+  $each(offlineStreamers, function(index, value) {
+
+  });
+}
+
+function populateOffline(data) {
+  $
 }
