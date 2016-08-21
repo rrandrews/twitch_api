24936664de5c22fcbd52ebe6b480edc5ba7ba00a
Ron Andrews <ron.andrews@gmail.com>
2016-08-21 08:10:37 -0400

    added styling to list and fixed bugs

--- twitch_api.js
+++ twitch_api.js
@@ -1,18 +1,17 @@
 streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck",
-            "habathcx", "RobotCaleb", "noobs2ninjas", "LotharHS", "brunofin"];
+            "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
 
 $(document).ready(function() {
 
   $.when(queryTwitch(streamers)).then(
     function(data) { updateTable(data); },
-    function(data) { alert(data.status); }
+    function(fail) { alert(fail.status); }
   );
 
 });
 
 function queryTwitch(channels) {
   query = "https://api.twitch.tv/kraken/streams?channel=" + channels.join();
-  console.log(query);
   return $.ajax(query);
 }
 
@@ -27,7 +26,8 @@ function updateTable(data) {
   var html = '';
   $.each(data.streams, function(index, value) {
     offlineStreamers.splice(offlineStreamers.indexOf(this.channel.display_name), 1);
-    html += '<tr><td><img class="logo" src="' + this.channel.logo + '"></td>' +
+    html += '<tr class="online"><td><img class="logo" src="' +
+            this.channel.logo + '"></td>' +
             '<td><a href="' + this.channel.url + '">' +
             this.channel.display_name + '</a></td>' +
             '<td>' + this.channel.status + '</td></tr>';
@@ -38,11 +38,11 @@ function updateTable(data) {
     html += '<tr><td></td><td>';
     $.when(queryChannel(channelName)).then(
       function(data) {
-        $("#streams-tbody").append('<tr><td></td><td>' +
-                  '<a href="' + data._links.channel + '">' + channelName +
-                  '</a></td><td>Offline</td></tr>'); },
-      function(failure) {
-        $("#streams-tbody").append('<tr><td></td><td>' + channelName +
+        $("#streams-tbody").append('<tr class="offline"><td></td><td>' +
+                  '<a href="http://www.twitch.tv/' + channelName + '">' +
+                  channelName + '</a></td><td>Offline</td></tr>'); },
+      function(fail) {
+        $("#streams-tbody").append('<tr class="dead"><td></td><td>' + channelName +
                   '</td><td>Channel does not exist!</td></tr>'); }
     );
   });
